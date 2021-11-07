79e5eafe1439cbc565ccf0bd6b6bc52339e32633
lissalad <elissalaymon@gmail.com>
2021-11-06 20:22:29 -0700

    comments added

--- app.py
+++ app.py
@@ -2,49 +2,49 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
+import certifi
 
 host = os.environ.get("DB_URL")
-client = MongoClient(host)
+client = MongoClient(host, tlsCAFile=certifi.where())
+
 db = client.Playlister
 playlists = db.playlists
+comments = db.comments
 
 app = Flask(__name__)
 # cool!!
-def video_url_creator(id_lst):
-    videos = []
-    for vid_id in id_lst:
-        # We know that embedded YouTube videos always have this format
-        video = 'https://youtube.com/embed/' + vid_id
-        videos.append(video)
-    return videos
 
-# @app.route('/')
-# def index():
-#     """Return homepage."""
-#     return render_template('home.html', msg='Flask is Cool!!')
-
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
-
+# ----------- PAGES ----------------------------------------- #
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    playlist_comments = comments.find({'playlist_id': playlist_id})
+
+    print(playlist_comments[0])
+    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
+
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    print('help')
     return render_template('playlists_new.html')
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    # Add the title parameter here
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
-    """Submit a new playlist."""
-    # Grab the video IDs and make a list out of them
     video_ids = request.form.get('video_ids').split()
-    # call our helper function to create the list of links
     videos = video_url_creator(video_ids)
     playlist = {
         'title': request.form.get('title'),
@@ -54,23 +54,7 @@ def playlists_submit():
     }
     playlists.insert_one(playlist)
     return redirect(url_for('playlists_index'))
-                                  
-# @app.route('/playlists/<playlist_id>')
-# def get_playlist(playlist_id):
-#     return 'My ID is ' + playlist_id
 
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
-
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    # Add the title parameter here
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -98,20 +82,27 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 
+# ------------------ HELPER ------------------------------------- #
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
 
-# Add this header to distinguish Comment routes from Playlist routes
-########## COMMENT ROUTES ##########
+
+# --------------- COMMENT ROUTES ---------------------------- #
 
 @app.route('/playlists/comments', methods=['POST'])
 def comments_new():
     """Submit a new comment."""
-    # TODO: Fill in the code here to build the comment object,
-    # and then insert it into the MongoDB comments collection
-    comments={
+    comment={
       'playlist_id': request.form.get('playlist_id'),
       'title': request.form.get('title'),
-      'description': request.form.get('description')
+      'description': request.form.get('content')
       }
+    comments.insert_one(comment)
     return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 
